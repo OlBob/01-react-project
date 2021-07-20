727f57da7c2be84b4777c32ab17e33c9611f5a01
OlBob <cerebrum94@gmail.com>
2021-07-20 11:09:00 +0300

    made components from main blocks

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img className="logo" src="https://rayamarketing.com/uploads/blog/Animated-Logo-Design/Animated-Logo-Design.logo-social-sq_1612376226.png" alt=""/>
+    </header>
+  )
+}
+
+export default Header;
