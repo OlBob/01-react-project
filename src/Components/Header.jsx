0c47febda60819180d276f3cfed308e2a5bc5922
OlBob <cerebrum94@gmail.com>
2021-07-20 12:24:35 +0300

    remade styles with css-modules

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import style from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header className="header">
-      <img className="logo" src="https://rayamarketing.com/uploads/blog/Animated-Logo-Design/Animated-Logo-Design.logo-social-sq_1612376226.png" alt=""/>
+    <header className={style.header}>
+      <img className={style.logo} src="https://rayamarketing.com/uploads/blog/Animated-Logo-Design/Animated-Logo-Design.logo-social-sq_1612376226.png" alt=""/>
     </header>
   )
 }
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import style from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={style.profile}>
       <div>
         <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt=""/>
       </div>
@@ -11,9 +12,9 @@ const Profile = () => {
       </div>
       <div>My posts</div>
       <div>New post</div>
-      <div>
-        <div>Post 1</div>
-        <div>Post 2</div>
+      <div className={style.posts}>
+        <div className={style.item}>Post 1</div>
+        <div className={style.item}>Post 2</div>
       </div>
     </div>
   )
