e91da47c219fa503d43354dddbf68f65bbc01dff
OlBob <cerebrum94@gmail.com>
2021-07-26 16:02:21 +0300

    added dialogs(Messages) content

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -3,8 +3,38 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return (
-    <div className="dialogs">
-      <h1>Dialogs</h1>
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>
+          Alex
+        </div>
+        <div className={s.dialog}>
+          Pyotr
+        </div>
+        <div className={s.dialog}>
+          Max
+        </div>
+        <div className={s.dialog}>
+          Mustafa
+        </div>
+        <div className={s.dialog}>
+          Lily
+        </div>
+        <div className={s.dialog}>
+          Leo
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>
+          <p>Hi</p>
+        </div>
+        <div className={s.message}>
+          <p>How is your day?</p>
+        </div>
+        <div className={s.message}>
+          <p>What's up?</p>
+        </div>
+      </div>
     </div>
   )
 }
