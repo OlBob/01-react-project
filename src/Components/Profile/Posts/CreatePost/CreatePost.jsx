2c3a2724df981036329e687461f85bed276375c2
OlBob <cerebrum94@gmail.com>
2021-07-26 15:40:30 +0300

    added NavLink to prevent page reload, added class 'active' to NavLink

--- src/Components/Profile/Posts/CreatePost/CreatePost.jsx
+++ src/Components/Profile/Posts/CreatePost/CreatePost.jsx
@@ -5,7 +5,7 @@ import style from "./CreatePost.module.css";
 const CreatePost = () => {
   return (
     <div className={style.myPost}>
-      <textarea name="Message" rows="5">Message</textarea>
+      <textarea name="Message" rows="5"></textarea>
       <div className={style.buttons}>
         <button className={style.submit}>Submit</button>
         <button className={style.delete}>Delete</button>
