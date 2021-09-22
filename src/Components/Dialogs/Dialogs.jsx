dca941dc20c6a908174afa298893726b60e8b510
OlBob <cerebrum94@gmail.com>
2021-09-22 14:17:03 +0300

    updated map dialogs and map posts

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -14,37 +14,38 @@ const Message = (props) => {
 
 const Dialogs = (props) => {
 
-  let dialogsData = [
+  let dialogs = [
     {id: 1, name: "Alex"},
     {id: 2, name: "Pyotr"},
     {id: 3, name: "Max"},
     {id: 4, name: "Mustafa"},
     {id: 5, name: "Lily"},
     {id: 6, name: "Leo"},
-  ];
+  ],
 
-  let messagesData =[
+  messages =[
     {id: 1, message: "Hi"},
     {id: 2, message: "How is your day?"},
     {id: 3, message: "What's up?"},
     {id: 4, message: "I'm rich"},
     {id: 5, message: "Ololo"},
     {id: 6, message: "I'm new here!"},
-  ]
+  ];
+
+  let dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id} /> ),
+      messagesElements = messages.map( m => <Message message={m.message} /> );
 
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
         {
-          dialogsData.map((dialog) =>
-            <DialogItem name={dialog.name} id={dialog.id} />
-        )}
+          dialogsElements
+        }
       </div>
       <div className={s.messages}>
         {
-          messagesData.map(content =>
-            <Message message={content.message} />
-        )}
+          messagesElements
+        }
       </div>
     </div>
   )
--- src/Components/Profile/Posts/Posts.jsx
+++ src/Components/Profile/Posts/Posts.jsx
@@ -5,10 +5,12 @@ import CreatePost from "./CreatePost/CreatePost";
 
 const Posts = () => {
 
-  let postsData = [
+  let posts = [
     {id: 1, message: "Hi, how are you?", likesCount: 15,},
     {id: 2, message: "It's my first post", likesCount: 20,}
   ]
+
+  let postsElements = posts.map( p => <Post key={p.id} message={p.message} likesCount={p.likesCount}/> )
 
   return (
     <div className={style.posts}>
@@ -16,9 +18,7 @@ const Posts = () => {
       <CreatePost/>
       <div className={style.publishedPosts}>
         {
-          postsData.map(content =>
-            <Post key={content.id} message={content.message} likesCount={content.likesCount}/>
-          )
+          postsElements
         }
       </div>
     </div>
