f752497fa785c532702ac88cc1af23bd936b7a24
OlBob <cerebrum94@gmail.com>
2022-02-03 11:36:48 +0200

    added authorization check

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
-import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
-import Profile from "./Components/Profile/Profile";
 import { Route } from "react-router-dom";
 import News from "./Components/News/News";
 import Music from "./Components/Music/Music";
@@ -10,13 +8,14 @@ import Settings from "./Components/Settings/Settings";
 import DialogsContainer from "./Components/Dialogs/DialogsContainer";
 import UsersContainer from "./Components/Users/UsersContainer";
 import ProfileContainer from "./Components/Profile/ProfileContainer";
+import HeaderContainer from "./Components/Header/HeaderContainer";
 
 const App = (props) => {
   let { profilePage, dialogsPage, navbar } = props.store.getState();
 
   return (
     <div className="app-wrapper">
-      <Header />
+      <HeaderContainer />
       <Navbar navbar={navbar} />
       <div className="app-wrapper-content">
         <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
